22752cbd51bfa298de69b0e3e937bb627f6c777d
Shantanu-Tiwari <shantanutiwari240704@gmail.com>
2025-01-05 11:53:20 +0530

    mobile functionality

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+const socket = io();
+
 const canvas = document.getElementById("canvas");
 canvas.width = 0.98 * window.innerWidth;
 canvas.height = window.innerHeight;
@@ -23,26 +25,51 @@ function getPosition(event) {
     }
 }
 
-function startDrawing(event) {
-    event.preventDefault();
-    const pos = getPosition(event);
+canvas.addEventListener("mousedown", (e) => {
+    const pos = getPosition(e);
     drawing = true;
     x = pos.x;
     y = pos.y;
-}
+    socket.emit("start", { x, y });
+});
 
-function draw(event) {
+canvas.addEventListener("mousemove", (e) => {
     if (!drawing) return;
-    const pos = getPosition(event);
+    const pos = getPosition(e);
     drawLine(x, y, pos.x, pos.y, "black");
+    socket.emit("draw", { x1: x, y1: y, x2: pos.x, y2: pos.y });
     x = pos.x;
     y = pos.y;
-}
+});
 
-function stopDrawing() {
+canvas.addEventListener("mouseup", () => {
     drawing = false;
     ctx.beginPath();
-}
+});
+
+canvas.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    const pos = getPosition(e);
+    drawing = true;
+    x = pos.x;
+    y = pos.y;
+    socket.emit("start", { x, y });
+});
+
+canvas.addEventListener("touchmove", (e) => {
+    e.preventDefault();
+    if (!drawing) return;
+    const pos = getPosition(e);
+    drawLine(x, y, pos.x, pos.y, "black");
+    socket.emit("draw", { x1: x, y1: y, x2: pos.x, y2: pos.y });
+    x = pos.x;
+    y = pos.y;
+});
+
+canvas.addEventListener("touchend", () => {
+    drawing = false;
+    ctx.beginPath();
+});
 
 function drawLine(x1, y1, x2, y2, color = "black") {
     ctx.strokeStyle = color;
@@ -54,10 +81,10 @@ function drawLine(x1, y1, x2, y2, color = "black") {
     ctx.stroke();
 }
 
-canvas.addEventListener("mousedown", startDrawing);
-canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mouseup", stopDrawing);
+socket.on("start", (data) => {
+    ctx.moveTo(data.x, data.y);
+});
 
-canvas.addEventListener("touchstart", startDrawing);
-canvas.addEventListener("touchmove", draw);
-canvas.addEventListener("touchend", stopDrawing);
+socket.on("draw", (data) => {
+    drawLine(data.x1, data.y1, data.x2, data.y2, "black");
+});
